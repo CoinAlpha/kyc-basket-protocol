45905d047a12f3099b1f49cce6c4564803b5a818
Carlo P. Las Marias <carlol@gmail.com>
2018-10-08 15:05:51 -0700

    (refactor) update deployments to specify Deployer and KYC Admin

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
+const FEE_MULTIPLIER = 0;               // Set to 0 for zero fees
+
 module.exports = {
   // Gas price used for development and testing
   GAS_PRICE_DEV: 20e9,
   GAS_LIMIT: 5e6,
 
+  // Addresses
+  DEPLOYER_ADDRESS: '<INSERT_DEPLOYER_ADDRESS>',
+  KYC_ADMIN: '<INSERT_KYC_ADMIN_ADDRESS>',
+
   // Fee amounts used for development and testing
-  TRANSACTION_FEE: 0.005 * 1e18,        // Charge 0.5% transaction fee
-  PRODUCTION_FEE: 0.3 * 1e18,           // Charge 0.3 ETH per basket creation
-  SWAPPABLE_PRODUCTION_FEE: 0.3 * 1e18, // Charge 0.5 ETH per basket creation
-  ARRANGER_FEE: 0.01 * 1e18,            // Charge 0.01 ETH per basket minted
+  FEE_MULTIPLIER: 0,                                     // Set to 0 for zero fees
+  TRANSACTION_FEE: FEE_MULTIPLIER * 0.005 * 1e18,        // Charge 0.5% transaction fee
+  PRODUCTION_FEE: FEE_MULTIPLIER * 0.3 * 1e18,           // Charge 0.3 ETH per basket creation
+  SWAPPABLE_PRODUCTION_FEE: FEE_MULTIPLIER * 0.3 * 1e18, // Charge 0.5 ETH per basket creation
+  ARRANGER_FEE: FEE_MULTIPLIER * 0.01 * 1e18,            // Charge 0.01 ETH per basket minted
   FEE_DECIMALS: 18,
 
   // Zero address
